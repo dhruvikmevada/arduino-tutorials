b87fd4926bee711964cb3ab629a7021ea9a4f422
Dhruvik Mevada <dhruvikmevada@users.noreply.github.com>
2020-03-21 16:20:24 +0530

    Arduino basic program

--- 15-03-2019.ino
+++ 15-03-2019.ino
@@ -4,21 +4,19 @@ int blue_pin=9;
 int red_pin=8;
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(blue_pin,OUTPUT);
   pinMode(red_pin,OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-//  digitalWrite(blue_pin,HIGH);
-//  delay(300);
-//  digitalWrite(red_pin,HIGH);
+  digitalWrite(blue_pin,HIGH);
+  delay(300);
+  digitalWrite(red_pin,HIGH);
   
   digitalWrite(blue_pin,LOW);
-//  delay(300);
+  delay(300);
   digitalWrite(red_pin,LOW);
   
 }
